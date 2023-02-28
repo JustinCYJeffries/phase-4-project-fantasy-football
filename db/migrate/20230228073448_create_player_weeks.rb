35a9b26696cd7cd915676a01a7df1cd95e7cd23d
Justin Jeffries <justincyjeffries@gmail.com>
2023-02-28 02:43:11 -0500

    adds team controller

--- db/migrate/20230228073448_create_player_weeks.rb
+++ db/migrate/20230228073448_create_player_weeks.rb
@@ -1,9 +1,18 @@
 class CreatePlayerWeeks < ActiveRecord::Migration[6.1]
   def change
     create_table :player_weeks do |t|
-      t.integer :yards
-      t.integer :touchdowns
-      t.integer :interceptions
+      t.integer :receiving_yards
+      t.integer :receiving_td
+      t.integer :receptions
+      t.integer :rushing_yards
+      t.integer :rushing_td
+      t.integer :passing_yards
+      t.integer :passing_td
+      t.integer :passing_interceptions
+      t.integer :two_pt_conversion
+      
+      
+      
       t.belongs_to :player, null: false, foreign_key: true
       t.belongs_to :week, null: false, foreign_key: true
 
