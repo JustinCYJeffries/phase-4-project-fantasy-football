2c6be7d5d6911187b2d1c6655e45863da8b31449
Justin Jeffries <justincyjeffries@gmail.com>
2023-03-02 08:36:19 -0500

    adds add player button

---
+++ client/src/AddPlayerButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddPlayerButton = ({ onClick }) => {
+  return (
+    <button onClick={onClick}>
+      Add Player
+    </button>
+  );
+};
+
+export default AddPlayerButton;
