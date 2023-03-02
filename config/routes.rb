29a9c3068bfd7fed708ecf95033207f99686f5ce
Justin Jeffries <justincyjeffries@gmail.com>
2023-03-02 06:58:24 -0500

    team routes added

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,25 @@ Rails.application.routes.draw do
   get '/teams', to: 'teams#index'
   post '/users', to: 'users#create'
 
+  post '/teams' do
+    content_type :json
+  
+    # parse the request body to get the team name
+    request.body.rewind
+    team_name = JSON.parse(request.body.read)['name']
+  
+    # create a new team and return its data
+    team = Team.create(name: team_name, user_id: session[:user_id])
+    team.to_json
+  end
+  
+  # get all teams for the current user
+  get '/teams' do
+    content_type :json
+  
+    # get all teams for the current user and return their data
+    teams = Team.where(user_id: session[:user_id])
+    teams.to_json
+  end
 
 end
