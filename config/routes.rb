fdb26b5cc307990b422a6e986a2a03d73e24d4ae
Justin Jeffries <justincyjeffries@gmail.com>
2023-03-09 02:31:14 -0500

    fixes routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :users, only: [:create]
   resources :sessions, only: [:create, :destroy]
-  resources :teams
+  resources :teams do 
+    resources :players
+    resources :player_teams, only: [:index, :create]
+  end
   resources :players, only: [:index]
   resources :player_teams
   resources :sessions, only: [:create, :destroy]
