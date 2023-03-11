71d20c5097c172e3dded20db26aaf1f8ee02112e
Justin Jeffries <justincyjeffries@gmail.com>
2023-03-11 02:30:07 -0500

    clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   resources :users, only: [:create]
-  resources :sessions, only: [:create, :destroy]
+  resource :sessions, only: [:create, :destroy]
   resources :teams do
     resources :players
     resources :player_teams, only: [:index, :create, :destroy, :update]
   end
   resources :players, only: [:index, :create]
-  resources :player_teams
-  delete '/sessions', to: 'sessions#destroy'
+
+  
 
 end
